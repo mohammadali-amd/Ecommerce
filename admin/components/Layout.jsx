d66daf2be2f7ded92dc3e628873b69ae17d50385
mohammadali-amd <amidi1380@gmail.com>
2023-11-03 20:13:10 +0330

    Make dashboard responsive

--- admin/components/Layout.jsx
+++ admin/components/Layout.jsx
@@ -1,15 +1,18 @@
 import { useSession, signIn } from "next-auth/react";
 import Navbar from '@/components/Navbar';
 import { Inter } from 'next/font/google';
+import { useState } from "react";
+import Logo from "./Logo";
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Layout({ children }) {
+   const [showNave, setShowNave] = useState(false);
    const { data: session } = useSession();
    if (!session) {
       return (
          <main
-            className={`bg-blue-900 min-h-screen h-56 grid justify-center content-center ${inter.className}`}
+            className={`bg-[#FBFAFD] min-h-screen h-56 grid justify-center content-center ${inter.className}`}
          >
             <div className="bg-white grid gap-3 rounded-lg shadow-xl p-16">
                <h2 className="flex gap-2 justify-center text-blue-900 text-xl font-semibold mb-6">
@@ -26,9 +29,21 @@ export default function Layout({ children }) {
 
    }
    return (
-      <div className="bg-blue-900 min-h-screen flex">
-         <Navbar />
-         <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">{children}</div>
+      <div className="bg-[#FBFAFD] min-h-screen">
+         <div className="flex items-center p-4 md:hidden">
+            <span onClick={() => setShowNave(true)} className="cursor-pointer">
+               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+               </svg>
+            </span>
+            <div className="flex grow justify-center mr-6">
+               <Logo />
+            </div>
+         </div>
+         <div className="flex">
+            <Navbar show={showNave} close={setShowNave} />
+            <div className="flex-grow p-4">{children}</div>
+         </div>
       </div>
    )
 }
