fbeb0309e4a0696445a2582cefe5b6e558a55210
mohammadali-amd <amidi1380@gmail.com>
2023-11-28 23:56:05 +0330

    Update orders.js

--- admin/pages/orders.js
+++ admin/pages/orders.js
@@ -18,18 +18,30 @@ const OrdersPage = () => {
             <thead>
                <tr>
                   <th>Date</th>
+                  <th>Paid</th>
                   <th>Recipient</th>
                   <th>Products</th>
                </tr>
             </thead>
             <tbody>
                {orders.length > 0 && orders.map(order => (
-                  <tr>
+                  <tr className='border-b border-gray-200'>
                      <td>{(new Date(order.createdAt)).toLocaleString()}</td>
+                     <td className={'flex justify-center ' + order.paid ? 'text-green-600' : 'text-red-600'}>
+                        {order.paid ? 'YES' : 'NO'}
+                     </td>
                      <td>
-                        {order.name} {order.email} <br />
-                        {order.city} {order.postalCode} {order.Country} <br />
-                        {order.streetAddress}
+                        <div className='flex justify-center'>
+                           {order.name} {order.email}
+                        </div>
+                        <div className='flex justify-center'>
+                           {order.city} {order.postalCode} {order.Country}
+                        </div>
+                        <div className='flex justify-center'>
+                           <span className='md:w-3/5 text-center'>
+                              {order.streetAddress}
+                           </span>
+                        </div>
                      </td>
                      <td>
                         {order.line_items.map(item => (
